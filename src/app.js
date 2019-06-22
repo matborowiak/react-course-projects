45301eba4934189c7dc59c0b73c13cb864303037
Mateusz Borowiak <m.borowiak@hotmail.com>
2019-06-22 20:48:46 +0200

    Add JSX structure

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
-console.log('app.js is running')
-
 // JSX - Javascript XML
-var template = <p>Does this change?</p>
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
+    </div>
+)
+
 var appRoot = document.getElementById('app')
 
 ReactDOM.render(template, appRoot)
