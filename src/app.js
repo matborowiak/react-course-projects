e9db09024755d3596e881979dc0242606a88c73f
Mateusz Borowiak <m.borowiak@hotmail.com>
2019-07-09 22:23:56 +0200

    Added person.js and utils.js using webpack

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-console.log('app.js is running!!')
+// import './utils.js'
+// import { square, add } from './utils.js'
+
+// console.log('app.js is running!!')
+// console.log(square(4))
+// console.log(add(7,4))
+
+// person.js
+// named export isAdult(18) - true if adult otherwise false
+// named export canDrink(18) - true if 21 and over, otherwise false
+
+import { isAdult, canDrink } from './person.js'
+
+console.log(isAdult(18))
+console.log(canDrink(18))
---
+++ src/person.js
@@ -0,0 +1,3 @@
+export const isAdult = (age) => age > 17 ? true : false
+
+export const canDrink = (age) => age > 20 ? true : false
