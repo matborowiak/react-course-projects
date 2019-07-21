ff19606e23b93bd6ae0fc1862d7b330e681f75ff
Mateusz Borowiak <m.borowiak@hotmail.com>
2019-07-21 16:02:13 +0200

    Installed babel ES6 class properties plugin and made cleaner syntax, removed constructors

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,15 +5,34 @@ import Action from './Action'
 import Options from './Options'
 
 class IndecisionApp extends React.Component {
-    constructor(props) {
-        super(props)
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
-        this.handlePick = this.handlePick.bind(this)
-        this.handleAddOption = this.handleAddOption.bind(this)
-        this.handleDeleteOption = this.handleDeleteOption.bind(this)
-        this.state = {
-            options: props.options
+    state = {
+        options: []
+    }
+    handleDeleteOptions = () => {
+        this.setState(() => ({ options: [] }))
+    }
+    handleDeleteOption = (optionToRemove) => {
+        this.setState(prevState => ({
+            options: prevState.options.filter(
+                option => optionToRemove !== option
+            )
+        }))
+    }
+    handlePick = () => {
+        const randomNum = Math.floor(Math.random() * this.state.options.length)
+        const option = this.state.options[randomNum]
+        alert(option)
+    }
+    handleAddOption = (option) => {
+        if (!option) {
+            return 'Enter vaild value to add item'
+        } else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exists'
         }
+
+        this.setState(prevState => ({
+            options: prevState.options.concat([option])
+        }))
     }
     componentDidMount() {
         try {
@@ -33,33 +52,7 @@ class IndecisionApp extends React.Component {
             localStorage.setItem('options', json)
         }
     }
-    handleDeleteOptions() {
-        this.setState(() => ({ options: [] }))
-    }
-    handleDeleteOption(optionToRemove) {
-        this.setState(prevState => ({
-            options: prevState.options.filter(
-                option => optionToRemove !== option
-            )
-        }))
-    }
-
-    handlePick() {
-        const randomNum = Math.floor(Math.random() * this.state.options.length)
-        const option = this.state.options[randomNum]
-        alert(option)
-    }
-    handleAddOption(option) {
-        if (!option) {
-            return 'Enter vaild value to add item'
-        } else if (this.state.options.indexOf(option) > -1) {
-            return 'This option already exists'
-        }
-
-        this.setState(prevState => ({
-            options: prevState.options.concat([option])
-        }))
-    }
+    
     render() {
         const subtitle = 'Put your life in hands of a computer'
 
