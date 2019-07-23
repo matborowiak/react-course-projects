0a263a76c6f3229d2ba23402f030a1185f53d26d
Mateusz Borowiak <m.borowiak@hotmail.com>
2019-07-23 23:42:00 +0200

    Styled buttons

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,6 +3,7 @@ const Option = props =>  (
         <div>
             {props.optionText}
             <button
+                className="button button--link"
                 onClick={e => {
                     props.handleDeleteOption(props.optionText)
                 }}
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import Option from './Option'
 const Options = props => (
-        <div>
-            <button onClick={props.handleDeleteOptions}>Remove all</button>
-            {props.options.length === 0 && <p>Please add an option to get started!</p>}
-            {props.options.map(option => (
-                <Option
-                    key={option}
-                    optionText={option}
-                    handleDeleteOption={props.handleDeleteOption}
-                />
-            ))}
-        </div>
-    )
+    <div>
+        <button
+            className="button button--link"
+            onClick={props.handleDeleteOptions}
+        >
+            Remove all
+        </button>
+        {props.options.length === 0 && (
+            <p>Please add an option to get started!</p>
+        )}
+        {props.options.map(option => (
+            <Option
+                key={option}
+                optionText={option}
+                handleDeleteOption={props.handleDeleteOption}
+            />
+        ))}
+    </div>
+)
 
 export default Options
