f302428bce73594ae3f19e6ce07bd57ab590a9c4
Mateusz Borowiak <m.borowiak@hotmail.com>
2019-07-24 15:39:58 +0200

    Styled widget

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -73,12 +73,15 @@ class IndecisionApp extends React.Component {
                         hasOptions={this.state.options.length > 0}
                         handlePick={this.handlePick}
                     />
+                    <div className="widget">
                     <Options
                         options={this.state.options}
                         handleDeleteOptions={this.handleDeleteOptions}
                         handleDeleteOption={this.handleDeleteOption}
                     />
                     <AddOption handleAddOption={this.handleAddOption} />
+                    </div>
+                    
                 </div>
 
                 <OptionModal
